d41f0affe0964b09ee13b3e81be857a12e1b89db
Yago <yago.ks5@gmail.com>
2023-10-10 08:21:03 -0300

    adicionando outra pagina para pratica

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,14 @@
+export const dataFake = [
+  {
+    id: 1,
+    photo: 'aaa',
+    title: 'saiu novo filme',
+    description: 'jabutinis',
+  },
+  {
+    id: 2,
+    photo: 'aaa',
+    title: 'saiu novo filme',
+    description: 'ola',
+  },
+];
